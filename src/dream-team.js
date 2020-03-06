d915dcd39f7007e50bb4abc3cb6fa2770c903948
Roman Agarkov <agarkovromachka@mail.ru>
2020-03-06 07:56:55 +0300

    feat: solved

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,10 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (typeof members === "number" || members === false || members === null || members === undefined || members.keys === false) {
+    return false;
+  } else if (members.length > 0) {
+    let newArr = members.filter(word => typeof word === 'string').map(index => index.toUpperCase().replace(/\s/g, '').substr(0, 1))
+    return newArr.sort().join('').toUpperCase();
+  } else {
+    return false;
+  }
 };
